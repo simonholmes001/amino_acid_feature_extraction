819ee43684eeaf82f6ddad21932e69d11e964521
simonholmes001 <simonholmesabc@gmail.com>
2020-03-13 07:59:06 +0100

    Finish code for download of aaindex file from //ftp.genome.jp/pub/db/community/aaindex/

--- amino_acid_feature_extraction/test.sh
+++ amino_acid_feature_extraction/test.sh
@@ -2,10 +2,21 @@
 
 # Script to extract physcial-chemical information for amino acids
 
+echo Downloading amino acid physical-chemical features from //ftp.genome.jp/pub/db/community/aaindex/...
+
+python3 ftp_call.py
+
 DATA_PATH="../data"
 OUTPUT_PATH="../output"
 OUTPUT_FILE_1="predata.txt"
 OUTPUT_FILE="data.txt"
+
+rm -rf $OUTPUT_PATH
+rm -rf $DATA_PATH
+mkdir $DATA_PATH
+mkdir $OUTPUT_PATH
+
+mv aaindex1 $DATA_PATH
 
 echo Extracting index data...
 
@@ -16,3 +27,5 @@ do
 done
     mv $OUTPUT_FILE $OUTPUT_PATH"/"
 echo Extraction complete
+
+rm $OUTPUT_FILE_1
